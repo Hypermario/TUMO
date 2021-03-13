d344c15ce09ff83d7e9704ffc91549a0dd657d81
AXXIAR <70155662+AXXIAR@users.noreply.github.com>
2021-03-13 18:36:47 +0100

    Axxiar dev (#5)
    * Create README.md
    
    * maj + ajout
    
    j'ai rajouter la commande suggestion qu'on avait mit dans la branche main directe
    + ajout d'une commande pour étirer le texte
    
    * ajout d'une commande infos
    
    * Update events.py
    
    * ajout de commandes et +
    
    rajout d'un fichier dédié aux commandes qui altère du texte
    
    ajout des commandes :
    - `;arrondir` *alias ;ar*
    -`;carre` *alias ;c*
    - `;minuscule` *alias ;min*
    - `;majuscule` *alias ;maj*
    - `;emoji` *alias ;em*
    
    * Update textransform.py
    
    * Deplacement commande ;etirer
    
    suppression de la commande ;etirer qui a étée déplacée dans cogs/textransform.py
    
    Co-authored-by: Hypermario <molvautj@gmail.com>

--- cogs/events.py
+++ cogs/events.py
@@ -65,6 +65,36 @@ class Events(commands.Cog):
         await ctx.send(" ".join(texte))
 ####################################################################################
 #----------------------------------------------------------------------------------------------
+############################# INFOS ############################
+    @commands.command(aliases=['i'])
+    async def infos(self,ctx,*suggestion):
+        embed=discord.Embed(description="Hyperfort est un bot créé par <@175236651734269952> \n **__Commandes__** Voici les commandes qui sont à votre disposition : ", color=0xf8e71c)
+        embed.add_field(name=";infos\nalias ;i", value="*Renvoie des infos sur le bot et comment l'utiliser*",inline=True)
+        embed.add_field(name=";etirer [texte]\nalias ;et", value="*Renvoie votre texte avec des espaces entre chaque\nl e t t r e*",inline=True)
+        embed.add_field(name=";question [question]\nalias ;q", value="*Répond à votre question par :\noui/non/possible/tg*",inline=True)
+        embed.add_field(name=";suggest[suggestion] \nalias ;sg", value="*Renvoie votre suggestion avec les réactions :arrow_up_small: :arrow_down_small: pour permettre de voter*", inline=True)
+        embed.add_field(name=";reactionmessage \nalias ;rm", value="*Vous permet de créer différentes choses interactivement* \n ``réservé aux admins``", inline=True)
+        embed.add_field(name="**__GitHub__**", value="Retrouvez son code source ici : https://github.com/Hypermario/TUMO-bot", inline=False)
+        await ctx.send(embed=embed)
+        logging.warning("exec infos"))
+####################################################################################
+#----------------------------------------------------------------------------------------------
+############################# SUGGESTION ############################
+    @commands.command(aliases=['sg'])
+    async def suggest(self,ctx,*suggestion):
+        await ctx.message.delete(delay=5.0)      # à laisser (?)
+        
+        if (not suggestion):
+            await ctx.send('Pense à me donner ta suggestion', delete_after=10)
+        else:
+            setup=discord.Embed(title=" ".join(suggestion),colour=discord.Colour.gold())
+            setup.set_author(name=f'{ctx.author} suggère :', icon_url=f'{ctx.author.avatar_url}')
+            setupMessage = await ctx.send(embed=setup)    
+            await setupMessage.add_reaction('🔼')
+            await setupMessage.add_reaction('🔽')
+            logging.warning("exec Sg pour : "+" ".join(suggestion))
+####################################################################################
+#----------------------------------------------------------------------------------------------
 ############################ SETUP ############################
 def setup(client):
     client.add_cog(Events(client))
