7e9e4358cc5c56773470eb24d6eaf7bec48eb4d4
AXXIAR <70155662+AXXIAR@users.noreply.github.com>
2021-03-28 20:05:14 +0200

    suppression commande sg
    la commande suggest était en double

--- cogs/events.py
+++ cogs/events.py
@@ -79,22 +79,6 @@ class Events(commands.Cog):
         logging.warning("exec infos"))
 ####################################################################################
 #----------------------------------------------------------------------------------------------
-############################# SUGGESTION ############################
-    @commands.command(aliases=['sg'])
-    async def suggest(self,ctx,*suggestion):
-        await ctx.message.delete(delay=5.0)      # à laisser (?)
-        
-        if (not suggestion):
-            await ctx.send('Pense à me donner ta suggestion', delete_after=10)
-        else:
-            setup=discord.Embed(title=" ".join(suggestion),colour=discord.Colour.gold())
-            setup.set_author(name=f'{ctx.author} suggère :', icon_url=f'{ctx.author.avatar_url}')
-            setupMessage = await ctx.send(embed=setup)    
-            await setupMessage.add_reaction('🔼')
-            await setupMessage.add_reaction('🔽')
-            logging.warning("exec Sg pour : "+" ".join(suggestion))
-####################################################################################
-#----------------------------------------------------------------------------------------------
 ############################ SETUP ############################
 def setup(client):
     client.add_cog(Events(client))
